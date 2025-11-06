432c45b41175aa7f4385968c2459336d4d0344b1
marikmarie <mariamtukasingura8@gmail.com>
2025-11-06 10:19:55 +0300

    New dashboard

--- src/pages/customer/CustomerDashboardPage.tsx
+++ src/pages/customer/CustomerDashboardPage.tsx
@@ -1,17 +1,21 @@
+
 // src/pages/Customer/Dashboard.tsx
 import { useEffect, type JSX } from "react";
 import { useNavigate } from "react-router-dom";
 import CustomerDashboard from "../../features/customer/CustomerDashboard";
-import useSession from "../../hooks/useSession"; // ✅ use new session hook
+import useSession from "../../hooks/useSession";
 
 export default function CustomerDashboardPage(): JSX.Element {
-  const { isAuthenticated, user } = useSession();
+  const { isAuthenticated, user, loading: sessionLoading } = useSession();
   const navigate = useNavigate();
 
   useEffect(() => {
+    // wait until session finished loading
+    if (sessionLoading) return;
+
     // If NOT authenticated → go to login
     if (!isAuthenticated) {
-      navigate("/login",  { replace: true });
+      navigate("/login", { replace: true });
       return;
     }
 
@@ -19,12 +23,8 @@ export default function CustomerDashboardPage(): JSX.Element {
     if (user?.role && user.role !== "customer") {
       if (user.role === "vendor") navigate("/vendor/dashboard");
       else if (user.role === "admin") navigate("/admin");
-     // else navigate("/customer/dashboard")
     }
-  }, [isAuthenticated, user]);
-  //}, [isAuthenticated, user, navigate]);
+  }, [isAuthenticated, user, sessionLoading, navigate]);
 
-  return (
-  <CustomerDashboard />
-  );
+  return <CustomerDashboard />;
 }
